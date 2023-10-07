d7c2a755c772342f70cf830dc5199824ff9a2914
Victor Frolov-Bukanov <124510561+VictorFBI@users.noreply.github.com>
2023-10-07 23:01:19 +0300

    Update enumerate.h

--- enumerate/enumerate.h
+++ enumerate/enumerate.h
@@ -1,5 +1,6 @@
 #pragma once
-
+#include <functional>
+#include <utility>
 template <class Iterator>
 class IteratorRange {
 public:
@@ -14,6 +15,33 @@ public:
         return end_;
     }
 
-private:
+public:
     Iterator begin_, end_;
 };
+
+template <class T>
+class EnumerateIterator {
+public:
+    explicit EnumerateIterator(T ptr) : ptr_(ptr), idx_(0) {
+    }
+    auto operator!=(EnumerateIterator other) {
+        return ptr_ != other.ptr_;
+    }
+    auto operator++() {
+        ++ptr_;
+        ++idx_;
+        return *this;
+    }
+    auto operator*() {
+        return std::make_pair(idx_, std::ref(*ptr_));
+    }
+
+public:
+    T ptr_;
+    int idx_;
+};
+
+template <typename Container>
+auto Enumerate(Container& cont) {
+    return IteratorRange(EnumerateIterator(cont.begin()), EnumerateIterator(cont.end()));
+}
